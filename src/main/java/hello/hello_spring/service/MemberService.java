a5e2b941fee4a84574229356fc0ec0e8b87ace0d
yeonzeop <ksp2617@naver.com>
2025-02-19 11:57:23 +0900

    feat:MemberService Test Case 작성

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } // DI(Dependency Injection, 의존성 주입)
 
     /**
      * 회원가입
@@ -35,7 +39,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> fineOne(long memberId){
+    public Optional<Member> findOne(long memberId){
         return memberRepository.findById(memberId);
     }
 }
